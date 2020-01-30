90774ff8796492e067f443cb0666064c41150f30
Fatma Faruq <ffaruq@gmail.com>
2020-01-30 09:15:46 +0000

    adding new results

---
+++ prism/grid11.sh
@@ -0,0 +1,4 @@
+./bin/testrunoptions.sh grid 11r 
+sleep 60 
+sleep 60 
+./bin/testrunoptions.sh grid 11g 
---
+++ prism/wh.sh
@@ -0,0 +1,4 @@
+./bin/testrunoptions.sh warehouse1 r
+sleep 60 
+sleep 60 
+./bin/testrunoptions.sh warehouse1 g
